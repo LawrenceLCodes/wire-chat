11ba282b372259e14b0a95ee6bfae4a88a56c1c2
LawrenceL <lawman77@gmail.com>
2023-07-17 21:05:16 -0400

    changed code so that image and display name in navbar is dynamic based on current user who is logged in.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from '../Context/AuthContext';
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <span className="logo">Earth Alliance Messaging</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-        <span>Lawrence</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         {/* signOut function being called on listener is from firebase */}
         <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
